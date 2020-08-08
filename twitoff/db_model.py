2501657a75739b1c03ee7f795e99bc35336f1836
MarianVinas <marian-vinas@lambdastudents.com>
2020-08-08 15:42:11 -0700

    twitoff with HTML

--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -5,18 +5,22 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     follower_count = db.Column(db.Integer, nullable=False)
+    # Tweet IDs are ordinal ints, so we can fetch most recent tweets
+    newest_tweet_id = db.Column(db.BigInteger, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 
 class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     text = db.Column(db.Unicode(300))
-    # user = db.relationship('User', backref=db.backref('tweet', lazy=True))
+    embedding = db.Column(db.PickleType, nullable=False)
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('tweet', lazy=True))
 
     def __repr__(self):
         return '<Tweet %r>' % self.text
